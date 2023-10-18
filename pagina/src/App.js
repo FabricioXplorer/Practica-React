09c738d64cf165bb1ed1ceac033bfe3527232d25
FabricioXplorer <rendonfabricio21@gmail.com>
2023-10-18 09:29:17 -0400

    creando mi primer componente

---
+++ pagina/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Hola <code></code> Mundo.
+        </p>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mi primera App React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
